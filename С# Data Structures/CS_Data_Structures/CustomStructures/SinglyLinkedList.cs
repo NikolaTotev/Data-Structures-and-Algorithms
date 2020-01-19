45ec5374c9fabaf8ce5c0c7ba3a837ff5b0379be
Nikola <nikola.r.totev@gmail.com>
2020-01-19 11:50:42 +0200

    Started Singly Linked List.

---
+++ С# Data Structures/CS_Data_Structures/CustomStructures/SinglyLinkedList.cs
@@ -0,0 +1,58 @@
+﻿namespace CustomStructures
+{
+    public class SinglyLinkedList<T>
+    {
+        public SinglyLinkedListNode<T> Head { get; set; }
+        public SinglyLinkedListNode<T> Tail { get; set; }
+        public int Size { get; set; }
+
+        public SinglyLinkedList()
+        {
+            Head = null;
+            Tail = null;
+            Size = 0;
+        }
+
+        public SinglyLinkedList(T headData)
+        {
+            Head = new SinglyLinkedListNode<T>(headData);
+            Tail = Head;
+            Size = 1;
+        }
+
+        public void InsertAtStart(T data)
+        {
+            SinglyLinkedListNode<T> newHead = new SinglyLinkedListNode<T>(data);
+            if (Head != null)
+            {
+                newHead.Next = Head;
+                Head = newHead;
+                Size++;
+                return;
+            }
+
+            Head = newHead;
+            Tail = Head;
+            Size++;
+        }
+        public void InsertAtPosition(int pos, T data)
+        {
+
+        }
+        public void InsertAtEnd(T data)
+        {
+            SinglyLinkedListNode<T> newNode = new SinglyLinkedListNode<T>(data);
+            if (Size != 0)
+            {
+                Tail.Next = newNode;
+                Size++;
+                return;
+            }
+            Head = newNode;
+            Tail = Head;
+            Size++;
+        }
+
+
+    }
+}
---
+++ С# Data Structures/CS_Data_Structures/CustomStructures/SinglyLinkedListNode.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace CustomStructures
+{
+    public class SinglyLinkedListNode<T>
+    {
+        public T Data { get; set; }
+        public SinglyLinkedListNode<T> Next { get; set; }
+
+        public SinglyLinkedListNode(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+
+        public void AddNext(SinglyLinkedListNode<T> next)
+        {
+            if (next != this)
+            {
+                Next = next;
+            }
+        }
+    }
+}
